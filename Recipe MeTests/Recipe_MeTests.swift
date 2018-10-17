5ba505652c02c75db3634a22797646cedb26c17b
Jora <jora@joras-macbook-pro.local>
2018-10-17 13:46:44 +0300

    Initial Commit

---
+++ Recipe MeTests/Recipe_MeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Recipe_MeTests.swift
+//  Recipe MeTests
+//
+//  Created by Jora on 10/17/18.
+//  Copyright © 2018 Jora. All rights reserved.
+//
+
+import XCTest
+@testable import Recipe_Me
+
+class Recipe_MeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
